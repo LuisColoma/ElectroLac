0b2e4b9e49f8615648274dd2fa1e078aa6c8d4d5
LuisColoma <lcoloma06@gmail.com>
2021-11-08 21:17:51 -0600

    Ultima actualizacion de base de datos e inventario

--- ElectroLac/venta/apps/events/models.py
+++ ElectroLac/venta/apps/events/models.py
@@ -11,13 +11,23 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=25, verbose_name='Nombre', unique=True)
     slug = models.SlugField(editable=False)
     summary = models.TextField(max_length=255)
-    is_free = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', null=True, blank=True)
     views = models.PositiveIntegerField(default=0)
+    stock = models.IntegerField(default=0, verbose_name='Stock')
     Amount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    pvp = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Precio de venta')
+
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['id']
+
+
